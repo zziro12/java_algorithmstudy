737e54e73b8028c913de37a66ee7236b43f14d92
zziro12 <opers7@naver.com>
2020-01-11 13:00:29 +0900

    add [Multi,Div,Fourop.java}

---
+++ algorithm_practice/src/algorithm_practice/Div.java
@@ -0,0 +1,15 @@
+package algorithm_practice;
+
+import java.util.*;
+public class Div {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double a,b;
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+		System.out.println(a/b);
+		
+	}
+
+}
