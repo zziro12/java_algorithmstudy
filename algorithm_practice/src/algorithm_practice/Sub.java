885fdbb8702e1ccc0852368c1d61b3248c360aa8
zziro12 <opers7@naver.com>
2020-01-11 12:31:56 +0900

    add [Add.java & Sub.java]

---
+++ algorithm_practice/src/algorithm_practice/Sub.java
@@ -0,0 +1,12 @@
+package algorithm_practice;
+
+import java.util.*;
+public class Sub {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a-b);
+	}
+}
