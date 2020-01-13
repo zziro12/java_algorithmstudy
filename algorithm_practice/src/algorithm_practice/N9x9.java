998d3d7175a826fcaa7d659bd02c84ebf633ff94
zziro12 <opers7@naver.com>
2020-01-13 23:26:16 +0900

    Use for

---
+++ algorithm_practice/src/algorithm_practice/N9x9.java
@@ -0,0 +1,16 @@
+package algorithm_practice;
+
+import java.util.*;
+public class N9x9 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n;
+		n = sc.nextInt();
+		for(int i = 1; i<10;i++) {
+			System.out.println(n+" *"+ i +" = "+(n*i));
+		}
+
+	}
+
+}
