eff958ce8688a97f78ee1b3cf37ebc9d1977c8a3
zziro12 <opers7@naver.com>
2020-01-19 16:34:21 +0900

    Caseadd.java

---
+++ algorithm_practice/src/algorithm_practice/Caseadd.java
@@ -0,0 +1,18 @@
+package algorithm_practice;
+
+import java.util.*;
+public class Caseadd {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num , a, b;
+		num = sc.nextInt();
+		
+		for(int i = 0; i < num; i++) {
+			a = sc.nextInt();
+			b = sc.nextInt();
+			System.out.println(a+b);
+		}
+	}
+
+}
